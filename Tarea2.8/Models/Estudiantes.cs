817841be2484248c5833fd01dce8177b899920a6
anthonybarrera47 <anthony_barrera@ucne.edu.do>
2019-10-31 20:41:44 -0400

    Tareas 2.7 y 2.8 parcialmente

---
+++ Tarea2.8/Models/Estudiantes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tarea2._8.Models
+{
+    public class Estudiantes
+    {
+        public int EstudianteID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime Fecha_Inscripcion { get; set; }
+        public virtual ICollection<Inscripciones> Inscripciones { get; set; }
+    }
+    public class Inscripciones
+    {
+        public int InscripcionId { get; set; }
+        public int CursoId { get; set; }
+        public int EstudianteID { get; set; }
+        public int Semestre { get; set; }
+        public virtual Estudiantes Estudiante { get; set; }
+    }
+    public class Cursos
+    {
+        public int CursoID { get; set; }
+        public string Descripcion { get; set; }
+        public virtual ICollection<Inscripciones> Inscripciones { get; set; }
+
+    }
+
+}
